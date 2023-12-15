5e872aba5d769d24b77b8515e0988494a249d8d0
= <=>
2023-12-15 16:20:14 +0100

    Es el Controlador de centre y con su funcion store crea y guarda centros en la base de datos, despues reedirige a su funcion index para mostrar los centros en el view de centres entregando la variable  con todos los centros de la base de datos para poder mostrar-los

---
+++ ERIC/my-project/app/Http/Controllers/CentresController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Centre;
+use Illuminate\Http\Request;
+
+class CentresController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $centros = Centre::all();
+        return view('Admin.centres')
+            ->with('centros',$centros);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $centre = new Centre;
+        
+        $centre->name = $request->input('name');
+        $centre->address = $request->input('address');
+        $centre->cp = $request->input('cp');
+        $centre->city = $request->input('city');
+
+        $centre->save();
+
+
+        return redirect()->route('centres');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Centre $centre)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Centre $centre)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Centre $centre)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Centre $centre)
+    {
+        //
+    }
+}
