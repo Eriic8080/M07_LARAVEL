12fbd799a8480b68c852f728014396d1699ef008
= <=>
2023-12-17 13:01:42 +0100

    He agregado la funcion de edit que recibe la id del centro a modificar y la funcion update que modifica el centro con determinada id

--- ERIC/my-project/app/Http/Controllers/CentresController.php
+++ ERIC/my-project/app/Http/Controllers/CentresController.php
@@ -54,18 +54,29 @@ class CentresController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Centre $centre)
+    public function edit($id)
     {
-        //
+        // Busco el centro por su id que recibo del enlace
+        $centre = Centre::find($id);
+
+        // Me devuelve al fichero del formulario llevandome la variable $centre que lleva la id del centro
+        return view('Admin.modificarCentro')->with('centre', $centre);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Centre $centre)
+    public function update(Request $request, $id)
     {
-        //
+        // Busco otra vez el centro por su id que recibo del formulario(que es el que arrastra de la funcion edit)
+        $centre = Centre::find($id);
+
+        // Actualiza los datos del centro con los datos del formulario
+        $centre->update($request->all());
+
+        return redirect()->route('centres');
     }
+    
 
     /**
      * Remove the specified resource from storage.
